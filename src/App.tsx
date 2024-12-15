8c0041f11c4e205826af5fd2ac577964e1e9041b
Manisha varma <manishavkapex1@gmail.com>
2024-12-15 23:54:13 +0530

    challenge question

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import "./App.css";
 // import StateComponent from "./topics/03-state";
-import EventComponent from "./topics/04-event";
+// import EventComponent from "./topics/04-event";
+import Component from "./topics/05-challenge";
 // import Component from "./topics/02-props";
 
 function App() {
   return (
     <>
-
       {/* This is from 02-props 
 
       <Component name="Manisha" id={56}>
@@ -16,9 +16,9 @@ function App() {
 
       */}
 
-        {/* <StateComponent /> */}
-        <EventComponent />
-
+      {/* <StateComponent /> */}
+      {/* <EventComponent /> */}
+      <Component />
     </>
   );
 }
