7d288ac7ddbdc32739463a63d1af4ab959d8cd68
Manisha varma <manishavkapex1@gmail.com>
2024-12-17 11:39:38 +0530

    create initialstate and reducers

---
+++ src/topics/07-reducers/reducer.ts
@@ -0,0 +1,14 @@
+export type CounterState = {
+    count: number;
+    status: string
+}
+
+
+export const initialState: CounterState = {
+    count: 0,
+    status: 'pending'
+}
+
+export const counterReducer = (state: CounterState, action: any): CounterState => {
+    return state
+}
